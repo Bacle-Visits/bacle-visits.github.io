3f2efc4422ab535f3785d26d3dbcb96eb7b1e4cd
Thomas PUJOL <tpujol00@gmail.com>
2022-01-02 12:15:03 +0100

    header footer

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Accueil</router-link> |
-    <router-link to="/about">À propos</router-link>
-  </div>
+  <app-header />
   <router-view/>
+  <app-footer />
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import AppHeader from '@/components/AppHeader.vue'
+import AppFooter from '@/components/AppFooter.vue'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  components: {
+    AppHeader,
+    AppFooter
   }
 }
-</style>
+</script>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,40 @@
+<template>
+    <hr>
+    <footer>
+        <p>Copyright &copy; Thomas PUJOL 2021-{{ year }} </p>
+    </footer>
+</template>
+
+<script>
+import { computed } from 'vue'
+
+export default {
+  setup () {
+    const year = computed(() => new Date().getFullYear())
+    return { year }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
