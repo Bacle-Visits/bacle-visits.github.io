cd9865c5a417d84ff44b7204d6bda87a6745e939
Thomas PUJOL <tpujol00@gmail.com>
2022-01-02 11:20:40 +0100

    app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="nav">
-    <router-link to="/">Accueil</router-link> |
+    <router-link to="/">Accueil</router-link> | 
     <router-link to="/about">A propos</router-link>
   </div>
   <router-view/>
